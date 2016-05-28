78569bad10a97b0bbdf647ac778637d98d77dc67
bhnascar <bhnascar@gmail.com>
2016-05-28 11:14:16 -0700

    Adding some tweaks to learner.py, updated features.text, and partially completed segmentation extractor

---
+++ extractors/segmentation.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+from skimage import data, io, segmentation, color
+from skimage.future import graph
+from matplotlib import pyplot as plt
+
+from util import *
+
+def compute_diff(c1, c2):
+    """
+    Computes some diff between c1 and c2 for the
+    purpose of comparison.
+    """
+    return compute_sat_diff(c1, c2)# + compute_lightness_diff(c1, c2)
+
+def main():
+    #img = data.coffee()
+    img = io.imread("test_data/wonder.jpg")
+
+    labels1 = segmentation.slic(img, compactness=30, n_segments=100)
+    out1 = color.label2rgb(labels1, img, kind='avg')
+
+    # Convert to HSV space
+    out2 = color.rgb2hsv(out1)
+
+    max_color_diff = 0
+    max_pos = (-1, -1)
+
+    height, width, color_depth = out1.shape
+    for y in range(0, height - 1):
+        for x in range(0, width - 1):
+            self = out2[y, x, :]
+            bottom_neighbor = out2[y + 1, x, :]
+            right_neighbor = out2[y, x + 1, :]
+            
+            color_diff = compute_diff(self, bottom_neighbor)
+            if (color_diff > max_color_diff):
+                max_color_diff = color_diff
+                max_pos = (x, y)
+
+            color_diff = compute_diff(self, right_neighbor)
+            if (color_diff > max_color_diff):
+                max_color_diff = color_diff
+                max_pos = (x, y)
+
+    print "Max color diff ({}) at pos {}".format(max_color_diff, max_pos)
+
+    # Highlight max color diff position
+    for y in range(-10, 10):
+        for x in range(-10, 10):
+            py = max_pos[1] + y
+            px = max_pos[0] + x
+            if 0 <= py <= height and 0 <= px <= width:
+                #print "Marked pix"
+                out2[py, px, 0] = 1
+                out2[py, px, 1] = 0
+                out2[py, px, 2] = 0
+                out1[py, px, 0] = 255
+                out1[py, px, 1] = 0
+                out1[py, px, 2] = 0
+
+    #g = graph.rag_mean_color(img, labels1, mode='similarity')
+    #labels2 = graph.cut_normalized(labels1, g)
+    #out2 = color.label2rgb(labels2, img, kind='avg')
+
+    plt.figure()
+    io.imshow(out1)
+    plt.figure()
+    io.imshow(out2)
+    io.show()
+
+if __name__ == "__main__":
+    main()
--- learner.py
+++ learner.py
@@ -1,36 +1,63 @@
-'''
+#!/usr/bin/env python
+"""
 Reads in features, and prints out the trained weight vector.
-'''
+"""
+import sys
 import numpy as np
 import pandas as pd
-from sklearn import svm
 import matplotlib.pyplot as plt
-from sklearn.cross_validation import cross_val_predict
+
+from sklearn import svm
 from sklearn import linear_model
+from sklearn.cross_validation import cross_val_predict
 
-train_dataframe = pd.read_csv('output/features.txt')
-train_views = train_dataframe["views"].tolist()
-train_favs = train_dataframe["favorites"].tolist()
-train_features = train_dataframe.iloc[:,4:]
-train_features = np.array(train_features)
+def main(args):
+	# Read data
+	dataframe = pd.read_csv('output/features.txt')
+	
+	views = dataframe["views"].tolist()
+	favs = dataframe["favorites"].tolist()
+	
+	features = dataframe.iloc[:,4:]
+	features = np.array(features)
 
-# clf = svm.SVR()
-lr = linear_model.LinearRegression()
-# clf.fit(train_features, train_views)
+	# Partition into training and testing datasets (approx. half and half right now)
+	train_views = views[:500]
+	train_favs = favs[:500]
+	train_features = features[:500,:]
 
-# predicted = clf.predict(train_features)
-predicted = cross_val_predict(lr, train_features, train_views, cv=5)
-negative_indices = predicted < 0
-predicted[negative_indices] = 0
+	test_views = views[500:]
+	test_favs = favs[500:]
+	test_features = features[500:,:]
 
-score = np.sum(np.abs(np.divide(predicted - np.array(train_views),
-                                np.array(train_views))))
-print score
+	# SVR
+	rbf_svr = svm.SVR(kernel = "poly", C = 1e3, gamma = "auto")
+	rbf_svr.fit(train_features, train_views)
+	train_results = rbf_svr.predict(train_features)
+	predicted = rbf_svr.predict(test_features)
 
-'''plot'''
-fig, ax = plt.subplots()
-ax.scatter(train_views, predicted)
-ax.plot([min(train_views), max(train_views)], [min(train_views), max(train_views)], 'k--', lw=4)
-ax.set_xlabel('Measured')
-ax.set_ylabel('Predicted')
-plt.show()
+	# Linear regression
+	# lr = linear_model.LinearRegression()
+	# train_results = cross_val_predict(lr, train_features, train_views, cv=5)
+	# train_results[train_results < 0] = 0
+	# predicted = cross_val_predict(lr, test_features, test_views, cv=5)
+	# predicted[predicted < 0] = 0
+
+	train_score = np.sum(np.abs(np.divide(train_results - np.array(train_views),
+	                                	  np.array(train_views))))
+	test_score = np.sum(np.abs(np.divide(predicted - np.array(test_views),
+	                                	 np.array(test_views))))
+	print "Train score: {}".format(train_score)
+	print "Test score: {}".format(test_score)
+
+	# Plot output
+	fig, ax = plt.subplots()
+	ax.scatter(train_views, train_results, color="r")
+	ax.scatter(test_views, predicted, color="b")
+	ax.plot([min(train_views), max(train_views)], [min(train_views), max(train_views)], 'k--', lw=4)
+	ax.set_xlabel('Measured')
+	ax.set_ylabel('Predicted')
+	plt.show()
+
+if __name__ == "__main__":
+	main(sys.argv)
